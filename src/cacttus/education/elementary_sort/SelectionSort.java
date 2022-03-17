f9f10bad360d55c871067e45ca4c881ca024df10
Naim Sulejmani <naim.sulejmani@gmail.com>
2022-03-17 17:55:30 +0100

    Helper class & selection sort

--- src/cacttus/education/elementary_sort/SelectionSort.java
+++ src/cacttus/education/elementary_sort/SelectionSort.java
@@ -1,13 +1,15 @@
 package cacttus.education.elementary_sort;
 
 import cacttus.education.shared.Generator;
+import cacttus.education.shared.Helper;
 
 public class SelectionSort {
     public static void main(String[] args) {
         int[] array = Generator.generateArray(10, 1, 101);
         System.out.println("Vargu i pasortuar: ");
         Generator.printArray(array);
-        selectionSort(array);
+        //     selectionSort(array);
+        procedureSelectionSort(array);
         System.out.println("Vargu i sortuar: ");
         Generator.printArray(array);
     }
@@ -32,6 +34,24 @@ public class SelectionSort {
                 array[minIndex] = temp;
             }
         }
+    }
+
+    public static void procedureSelectionSort(int[] array) {
+        int min;
+        for (int i = 0; i < array.length - 1; i++) {
+            min = i; //supozojme se elementi ne poziten i eshte minimum
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[min] > array[j]) {
+                    min = j;
+                }
+            }
+
+            if (min != i) {
+                Helper.swap(array, min, i);
+            }
+        }
+
+
     }
 
 
---
+++ src/cacttus/education/shared/Helper.java
@@ -0,0 +1,10 @@
+package cacttus.education.shared;
+
+public class Helper {
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
